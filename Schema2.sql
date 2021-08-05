c5b1e01edb6699e9b035b53f43193674fe5a8629
Andrew <drewski1259@gmail.com>
2021-08-05 13:44:01 -0500

    update schema2, add demographic csv and ipynb

--- Schema2.sql
+++ Schema2.sql
@@ -1,13 +1,14 @@
 -- Creating tables
 CREATE TABLE Beer_Permit_Locations (
+	index VARCHAR NOT NULL
 	permit_number VARCHAR NOT NULL, 
 	permit_subtype VARCHAR NOT NULL,
-	date_entered VARCHAR NOT NULL,
-	date_issued VARCHAR NOT NULL, 
+	date_entered DATE NOT NULL,
+	date_issued DATE NOT NULL, 
 	business_name VARCHAR NOT NULL,
-	zip DATE NOT NULL,
-	latitude DATE NOT NULL, 
-	longitude INT NOT NULL,
+	zip VARCHAR NOT NULL,
+	latitude VARCHAR NOT NULL, 
+	longitude VARCHAR NOT NULL,
 	permit_duration VARCHAR NOT NULL, 
 	PRIMARY KEY (Permit_Number),
 	UNIQUE (Permit_Number)
